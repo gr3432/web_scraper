fda1c16c9521db5f9476f4baee630a2d5c95ab37
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-01-30 16:36:15 +0100

    assert that content has not changed

--- sajp.py
+++ sajp.py
@@ -5,3 +5,9 @@ url = 'https://sajp.info/applicationansokan/'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
+
+article = soup.find('article')
+
+previous_content = '\n\nApplication/Ansökan \n\n\xa0\nOnline application for JLPT Stockhlm in July 2019 will be open March 1, 2019.\n\n\xa0\nWebbanmälan för JLPT Stockholm i Juli 2019 öppnar 1 mars, 2019 .\n.\n\xa0\n\xa0\nDela det här:TwitterFacebookGoogleGillaGilla Laddar... \n'
+
+assert article.text == previous_content

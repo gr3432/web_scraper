e674506e11fa5e86e650949fa72a55b69bec157c
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-02-11 16:46:55 +0100

    boilerplate code

--- jcourse.py
+++ jcourse.py
@@ -1,1 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
 url = "https://www.universityadmissions.se/intl/search?period=1&freeText=Japanese&semesterPart=0"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, 'html.parser')

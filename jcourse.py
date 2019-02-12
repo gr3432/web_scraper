9f8c6ec07f77f8eaeedd10dff41a49dadfd75505
Georgios Rizothanasis <trivialgeorge@gmail.com>
2019-02-12 16:56:51 +0100

    get university names from li elements

--- jcourse.py
+++ jcourse.py
@@ -8,3 +8,10 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 search_results = soup.find('ul', class_='searchResult').find_all('li')
 
+names = [li.find_all('span', class_='appl_fontsmall') for li in search_results]
+
+# the first span contains the credits and the second the university name and location
+names = [name[1].text for name in names]
+
+print(names)
+
